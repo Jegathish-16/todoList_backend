b88e7f575e104a73458d923073e30f9c23056764
Jegathish-16 <jegathish162004@gmail.com>
2023-05-27 11:01:10 +0530

    Update include  nbc architecture for get method

--- Routes/todo_list.js
+++ Routes/todo_list.js
@@ -1,9 +1,13 @@
 // added REST Route operations
+// update ncp architectore fo get method
 const express = require('express')
-const router = express()
+const router = express.Router()
+const {getAllToDoItems} = require('../contorllers/todo_list')
 
-router.get('/',(require, response) =>{
-    response.send("List of all to-do items")
-})
+
+
+
+router.route('/').get(getAllToDoItems)
+
 
 module.exports = router;
---
+++ contorllers/todo_list.js
@@ -0,0 +1,5 @@
+const getAllToDoItems = (require, response) =>{
+    response.send("List of all to-do list")
+}
+
+module.exports = {getAllToDoItems};
